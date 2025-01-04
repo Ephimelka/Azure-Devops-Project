84792b94fa2d740242f1cdffad4b7869d7fb10bd
Ephrem Bulto <ephremmelkamu4@gmail.com>
2025-01-04 15:01:39 +0100

    second commit

--- Create_App_Service/variable.tf
+++ Create_App_Service/variable.tf
@@ -1,7 +1,7 @@
 # variables.tf
 variable "resource_group_name" {
   type    = string
-  default = "AbelTeraRG"
+  default = "EphiProject"
 }
 
 variable "location" {
@@ -11,10 +11,10 @@ variable "location" {
 
 variable "app_service_plan_name" {
   type    = string
-  default = "AbelASP"
+  default = "EphiASP"
 }
 
 variable "app_service_name" {
   type    = string
-  default = "Abel-Project2232"
+  default = "Ephi-DEVops"
 }
