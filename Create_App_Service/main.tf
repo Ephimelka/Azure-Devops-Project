8927d198273142119764456750f17258b2ef4c3d
Ephimelka <102452308+Ephimelka@users.noreply.github.com>
2025-01-15 15:26:03 +0100

    Update main.tf

--- Create_App_Service/main.tf
+++ Create_App_Service/main.tf
@@ -1,4 +1,3 @@
-# main.tf
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
@@ -14,11 +13,17 @@ resource "azurerm_app_service_plan" "appserviceplan" {
   }
 }
 
-resource "azurerm_app_service" "appservice" {
+resource "azurerm_linux_web_app" "appservice" {
   name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+  service_plan_id     = azurerm_app_service_plan.appserviceplan.id
+
+  site_config {
+    application_stack {
+      node_version = "20-lts"
+    }
+  }
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"
