64d3e1bc92c459249751442e7b11d1f7eaaf7da4
Ephimelka <102452308+Ephimelka@users.noreply.github.com>
2025-01-15 15:54:25 +0100

    Update main.tf

--- Create_App_Service/main.tf
+++ Create_App_Service/main.tf
@@ -7,6 +7,7 @@ resource "azurerm_app_service_plan" "appserviceplan" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
   sku {
     tier = "Standard"
     size = "S1"
